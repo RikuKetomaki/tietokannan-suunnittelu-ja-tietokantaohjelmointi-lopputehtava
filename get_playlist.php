176aa31bacadac4b0c850276f70938cb89ff8ee1
RikuKetomaki <riku.ketomaki@gmail.com>
2023-04-18 15:17:44 +0300

    Added

---
+++ get_playlist.php
@@ -0,0 +1,27 @@
+<?php
+
+try {
+
+    $playlist_id = 12;
+
+    $database = openDatabase();
+    $sql = "select tracks.Name, Composer from tracks inner join playlist_track on tracks.TrackId=playlist_track.TrackId inner join playlists on playlist_track.PlaylistId=playlists.PlaylistId where playlists.PlaylistId = $playlist_id";
+    $stmt = $database->prepare($sql);
+    $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // $sql = "select Composer from tracks inner join playlist_track on tracks.TrackId=playlist_track.TrackId inner join playlists on playlist_track.PlaylistId=playlists.PlaylistId where playlists.PlaylistId = $playlist_id";
+    // $stmt = $database->prepare($sql);
+    // $stmt->execute();
+    // $composer = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // header('HTTP/1.1 200 OK');
+    
+    foreach ($rows as $row) {
+        echo "<h3>".$row["Name"]."</h3>"."<p>".$row["Composer"]."</p>"."<br>";
+    }
+
+    
+} catch (PDOException $pdoex) {
+    returnErr($pdoex);
+}
