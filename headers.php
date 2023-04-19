4b61f84b55cd223533c162e6beac3261d67d8464
RikuKetomaki <riku.ketomaki@gmail.com>
2023-04-19 13:55:57 +0300

    removed application json

--- headers.php
+++ headers.php
@@ -3,7 +3,6 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: READ, CREATE, UPDATE, DELETE');
 header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, Access-Control-Allow-Header');
-header('Content-Type: application/json');
 header('Access-Control-Max-Age: 3600');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
