ffccbf0e687c638285b47ef770d121dbe64ba97f
RikuKetomaki <riku.ketomaki@gmail.com>
2023-04-19 13:56:06 +0300

    removed headers

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 
-require_once './functions.php';
-require_once './headers.php';
 
 //include './remove_invoice.php';
 //include './get_playlist.php';
 //include './remove_artist_info.php';
+include './get_artist_info.php';
