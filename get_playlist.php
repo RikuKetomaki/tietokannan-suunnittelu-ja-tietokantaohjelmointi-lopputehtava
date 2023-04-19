d42b10b6b9cf3e858e7c84ea3a5b83466dfe1286
RikuKetomaki <riku.ketomaki@gmail.com>
2023-04-19 13:55:42 +0300

    Added headers

--- get_playlist.php
+++ get_playlist.php
@@ -1,8 +1,10 @@
 <?php
+require_once './functions.php';
+require_once './headers.php';
 
 try {
 
-    $playlist_id = 12;
+    $playlist_id = 1;
 
     $database = openDatabase();
     $sql = "select tracks.Name, Composer from tracks inner join playlist_track on tracks.TrackId=playlist_track.TrackId inner join playlists on playlist_track.PlaylistId=playlists.PlaylistId where playlists.PlaylistId = $playlist_id";
@@ -13,7 +15,7 @@ try {
     // header('HTTP/1.1 200 OK');
     
     foreach ($rows as $row) {
-        echo "<h3>".$row["Name"]."</h3>"."<p>".$row["Composer"]."</p>"."<br>";
+        echo "<h3>".$row["Name"]."</h3>"."<p>".$row["Composer"]."</p>";
     }
 
     
--- remove_invoice.php
+++ remove_invoice.php
@@ -1,4 +1,6 @@
 <?php
+require_once './functions.php';
+require_once './headers.php';
 
 try {
 
