d0673f7c67c656adebeb5c42fdd12b22e0b9d337
RikuKetomaki <riku.ketomaki@gmail.com>
2023-04-19 12:27:25 +0300

    Added remove artist info

--- index.php
+++ index.php
@@ -3,4 +3,6 @@
 require_once './functions.php';
 require_once './headers.php';
 
-include './remove_invoice.php';
+//include './remove_invoice.php';
+//include './get_playlist.php';
+//include './remove_artist_info.php';
